275e8c60ddcfbc69391146fe8939f39e60c2e36e
NickLarsenNZ <nick@aptiv.co.nz>
2019-02-18 11:49:20 +1300

    VPC Creation
    ```
    aws_vpc.b: Creating...
      arn:                              "" => "<computed>"
      assign_generated_ipv6_cidr_block: "" => "false"
      cidr_block:                       "" => "192.168.128.0/17"
      default_network_acl_id:           "" => "<computed>"
      default_route_table_id:           "" => "<computed>"
      default_security_group_id:        "" => "<computed>"
      dhcp_options_id:                  "" => "<computed>"
      enable_classiclink:               "" => "<computed>"
      enable_classiclink_dns_support:   "" => "<computed>"
      enable_dns_hostnames:             "" => "<computed>"
      enable_dns_support:               "" => "true"
      instance_tenancy:                 "" => "default"
      ipv6_association_id:              "" => "<computed>"
      ipv6_cidr_block:                  "" => "<computed>"
      main_route_table_id:              "" => "<computed>"
      owner_id:                         "" => "<computed>"
      tags.%:                           "" => "2"
      tags.Lab:                         "" => "aws-route-lab"
      tags.Name:                        "" => "lab-vpc-a"
    aws_vpc.a: Creating...
      arn:                              "" => "<computed>"
      assign_generated_ipv6_cidr_block: "" => "false"
      cidr_block:                       "" => "192.168.0.0/17"
      default_network_acl_id:           "" => "<computed>"
      default_route_table_id:           "" => "<computed>"
      default_security_group_id:        "" => "<computed>"
      dhcp_options_id:                  "" => "<computed>"
      enable_classiclink:               "" => "<computed>"
      enable_classiclink_dns_support:   "" => "<computed>"
      enable_dns_hostnames:             "" => "<computed>"
      enable_dns_support:               "" => "true"
      instance_tenancy:                 "" => "default"
      ipv6_association_id:              "" => "<computed>"
      ipv6_cidr_block:                  "" => "<computed>"
      main_route_table_id:              "" => "<computed>"
      owner_id:                         "" => "<computed>"
      tags.%:                           "" => "2"
      tags.Lab:                         "" => "aws-route-lab"
      tags.Name:                        "" => "lab-vpc-a"
    aws_vpc.a: Creation complete after 4s (ID: vpc-060131da96b5468fd)
    aws_vpc.b: Creation complete after 4s (ID: vpc-052b4cd24853fc4d5)
    
    Apply complete! Resources: 2 added, 0 changed, 0 destroyed.
    
    ```

---
+++ vpc.tf
@@ -0,0 +1,17 @@
+resource "aws_vpc" "a" {
+    cidr_block = "192.168.0.0/17"
+
+    tags {
+      Name = "lab-vpc-a"
+      Lab = "aws-route-lab"
+    }
+}
+
+resource "aws_vpc" "b" {
+    cidr_block = "192.168.128.0/17"
+
+    tags {
+      Name = "lab-vpc-a"
+      Lab = "aws-route-lab"
+    }
+}
