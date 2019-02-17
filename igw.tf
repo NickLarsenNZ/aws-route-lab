6234a497a9239535c3ce6afaa8ed9ad600a96032
NickLarsenNZ <nick@aptiv.co.nz>
2019-02-18 11:52:15 +1300

    IGWs created
    aws_internet_gateway.b: Creating...
      owner_id:  "" => "<computed>"
      tags.%:    "0" => "2"
      tags.Lab:  "" => "aws-route-lab"
      tags.Name: "" => "lab-igw-b"
      vpc_id:    "" => "vpc-052b4cd24853fc4d5"
    aws_internet_gateway.a: Creating...
      owner_id:  "" => "<computed>"
      tags.%:    "0" => "2"
      tags.Lab:  "" => "aws-route-lab"
      tags.Name: "" => "lab-igw-a"
      vpc_id:    "" => "vpc-060131da96b5468fd"
    aws_internet_gateway.a: Creation complete after 2s (ID: 
    igw-0c65fec8609d53cae)
    aws_internet_gateway.b: Creation complete after 3s (ID: 
    igw-01df2e5f5eea03291)

---
+++ igw.tf
@@ -0,0 +1,17 @@
+resource "aws_internet_gateway" "a" {
+    vpc_id = "${aws_vpc.a.id}"
+
+    tags {
+      Name = "lab-igw-a"
+      Lab = "aws-route-lab"
+    }
+}
+
+resource "aws_internet_gateway" "b" {
+    vpc_id = "${aws_vpc.b.id}"
+
+    tags {
+      Name = "lab-igw-b"
+      Lab = "aws-route-lab"
+    }
+}
