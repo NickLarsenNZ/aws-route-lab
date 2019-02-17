29c6de2ebe15942fcbbedced1f4d5fdb455dafc7
NickLarsenNZ <nick@aptiv.co.nz>
2019-02-18 11:58:23 +1300

    Subnets created
    ```
    aws_subnet.b: Creating...
      arn:                             "" => "<computed>"
      assign_ipv6_address_on_creation: "" => "false"
      availability_zone:               "" => "<computed>"
      availability_zone_id:            "" => "<computed>"
      cidr_block:                      "" => "192.168.128.0/24"
      ipv6_cidr_block:                 "" => "<computed>"
      ipv6_cidr_block_association_id:  "" => "<computed>"
      map_public_ip_on_launch:         "" => "false"
      owner_id:                        "" => "<computed>"
      tags.%:                          "" => "2"
      tags.Lab:                        "" => "aws-route-lab"
      tags.Name:                       "" => "lab-subnet-b"
      vpc_id:                          "" => "vpc-052b4cd24853fc4d5"
    aws_subnet.a: Creating...
      arn:                             "" => "<computed>"
      assign_ipv6_address_on_creation: "" => "false"
      availability_zone:               "" => "<computed>"
      availability_zone_id:            "" => "<computed>"
      cidr_block:                      "" => "192.168.0.0/24"
      ipv6_cidr_block:                 "" => "<computed>"
      ipv6_cidr_block_association_id:  "" => "<computed>"
      map_public_ip_on_launch:         "" => "false"
      owner_id:                        "" => "<computed>"
      tags.%:                          "" => "2"
      tags.Lab:                        "" => "aws-route-lab"
      tags.Name:                       "" => "lab-subnet-a"
      vpc_id:                          "" => "vpc-060131da96b5468fd"
    aws_subnet.a: Creation complete after 1s (ID: subnet-09f38330a6bcf3097)
    aws_subnet.b: Creation complete after 2s (ID: subnet-060cca9a144074e5c)
    ```

---
+++ subnet.tf
@@ -0,0 +1,19 @@
+resource "aws_subnet" "a" {
+  vpc_id     = "${aws_vpc.a.id}"
+  cidr_block = "192.168.0.0/24"
+
+  tags {
+    Name = "lab-subnet-a"
+    Lab = "aws-route-lab"
+  }
+}
+
+resource "aws_subnet" "b" {
+  vpc_id     = "${aws_vpc.b.id}"
+  cidr_block = "192.168.128.0/24"
+
+  tags {
+    Name = "lab-subnet-b"
+    Lab = "aws-route-lab"
+  }
+}
