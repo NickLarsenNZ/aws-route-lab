5a912646124d06531df0802e7178cf750e40eeee
NickLarsenNZ <nick@aptiv.co.nz>
2019-02-18 16:44:43 +1300

    Moved to subnet variables because the aws_subnet resource doesn't output the CIDR for other resources to use.

--- subnet.tf
+++ subnet.tf
@@ -1,6 +1,15 @@
+variable "subnets" {
+  type = "map"
+
+  default = {
+    a = "192.168.0.0/24"
+    b = "192.168.128.0/24"
+  }
+}
+
 resource "aws_subnet" "a" {
   vpc_id     = "${aws_vpc.a.id}"
-  cidr_block = "192.168.0.0/24"
+  cidr_block = "${var.subnets["a"]}"
 
   tags {
     Name = "lab-subnet-a"
@@ -10,7 +19,7 @@ resource "aws_subnet" "a" {
 
 resource "aws_subnet" "b" {
   vpc_id     = "${aws_vpc.b.id}"
-  cidr_block = "192.168.128.0/24"
+  cidr_block = "${var.subnets["b"]}"
 
   tags {
     Name = "lab-subnet-b"
